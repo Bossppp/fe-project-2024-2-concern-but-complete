63dcb0e4cb7a9da80957a6995e55850f9655d922
knpmayy <kanokponbinhem@gmail.com>
2025-03-23 15:48:21 +0700

    feat: add profile settings

--- src/app/(user)/user/profile/page.tsx
+++ src/app/(user)/user/profile/page.tsx
@@ -1,8 +1,16 @@
+import ProfileSettingsForm from '@/components/ProfileSettingsForm'
+import UserNavigate from '@/components/UserNavigate'
 import React from 'react'
+
 
 function Profile() {
   return (
-    <div>Profile</div>
+    <div className="flex bg-white">
+          <UserNavigate />
+          <div className=" p-2 w-2/3">
+          <ProfileSettingsForm/>
+          </div>
+        </div>
   )
 }
 
