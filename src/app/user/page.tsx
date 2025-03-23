78fdf445cde7afc11772940261e6d99ff36f6a7b
knpmayy <kanokponbinhem@gmail.com>
2025-03-23 11:36:20 +0700

    feat: add icon for user navigate

---
+++ src/app/user/page.tsx
@@ -0,0 +1,12 @@
+import UserNavigate from '@/components/UserNavigate'
+import React from 'react'
+
+function user() {
+  return (
+    <div>
+        <UserNavigate/>
+    </div>
+  )
+}
+
+export default user
--- src/components/UserNavigate.tsx
+++ src/components/UserNavigate.tsx
@@ -1,27 +1,48 @@
-const UserNavigate: React.FC = () => {
+import Image from 'next/image';
+import { Calendar, House, LogOut, SquareUser } from "lucide-react";
+import React from 'react'
+
+function UserNavigate() {
   return (
     <div className="w-64 h-[515px] relative flex flex-col items-center">
-      <div className="w-44 h-44 bg-sky-800 rounded-full"></div>
-      {/**profile*/}
-    <div className="mt-2 justify-start text-sky-800 text-3xl font-semibold mb-10">Name of User</div>
-    <button className="w-64 h-16 bg-white border-l border-r border-t border-slate-400 flex items-center justify-start pl-8">
-      <div className="w-4 h-4 bg-sky-800 rounded-full mr-4"></div>
-      <span className="text-sky-800 text-xl font-semibold">Dashboard</span>
-    </button>
-    <button className="w-64 h-16 bg-white border-l border-r border-t border-slate-400 flex items-center justify-start pl-8">
-      <div className="w-4 h-4 bg-sky-800 rounded-full mr-4"></div>
-      <span className="text-sky-800 text-xl font-semibold">Profile Settings</span>
-    </button>
-    <button className="w-64 h-16 bg-white border-l border-r border-t border-slate-400 flex items-center justify-start pl-8">
-      <div className="w-4 h-4 bg-sky-800 rounded-full mr-4"></div>
-      <span className="text-sky-800 text-xl font-semibold">Manage Bookings</span>
-    </button>
-    <button className="w-64 h-16 bg-white border-1 border-slate-400 flex items-center justify-start pl-8">
-      <div className="w-4 h-4 bg-sky-800 rounded-full mr-4"></div>
-      <span className="text-sky-800 text-xl font-semibold">Log Out</span>
-    </button>
+      <div className="w-44 h-44 overflow-hidden rounded-full">
+        <Image
+          src="/img/mockProfile.jpg"
+          alt="Profile"
+          width={176}
+          height={176}
+          className="object-cover"
+        />
+      </div>
+      <div className="mt-2 justify-start text-sky-800 text-3xl font-semibold mb-10">
+        Name of User
+      </div>
+      <button className="w-64 h-16 bg-white border-l border-r border-t border-slate-400 flex items-center justify-start pl-7">
+        <House className="w-5 text-sky-800 mr-4" />
+        <span className="text-sky-800 text-xl font-semibold">
+          Dashboard
+          </span>
+      </button>
+      <button className="w-64 h-16 bg-white border-l border-r border-t border-slate-400 flex items-center justify-start pl-7">
+        <SquareUser className="w-5 text-sky-800 mr-4" />
+        <span className="text-sky-800 text-xl font-semibold">
+          Profile Settings
+        </span>
+      </button>
+      <button className="w-64 h-16 bg-white border-l border-r border-t border-slate-400 flex items-center justify-start pl-7">
+        <Calendar className="w-5 text-sky-800 mr-4" />
+        <span className="text-sky-800 text-xl font-semibold">
+          Manage Bookings
+        </span>
+      </button>
+      <button className="w-64 h-16 bg-white border-1 border-slate-400 flex items-center justify-start pl-7">
+        <LogOut className="w-5 text-sky-800 mr-4" />
+        <span className="text-sky-800 text-xl font-semibold">
+          Log Out
+          </span>
+      </button>
     </div>
-  );
-};
+  )
+}
 
-export default UserNavigate;
+export default UserNavigate
